bc7727505e1cf9441d166e579728ac0ec2674026
onmax <maximogarciamtnez@gmail.com>
2024-11-20 23:55:19 -0600

    log

--- app/stores/blocks.ts
+++ app/stores/blocks.ts
@@ -4,13 +4,13 @@ import { BlockType } from 'nimiq-rpc-client-ts'
 
 export const useBlocks = defineStore('blocks', () => {
   const url = `/api/blocks`
-  console.log('21312')
+  console.log('/api/block')
   const { status, data: blocksStr } = useWebSocket(url, {
     autoReconnect: {
       retries: 3,
       delay: 1000,
       onFailed() {
-        alert('Failed to connect to Stats WS after 3 retries')
+        alert('Failed to connect to WS after 3 retries')
       },
     },
   })
