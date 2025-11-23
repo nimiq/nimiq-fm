4d47cb0c258118102f1fde90d5f3a2f259424617
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2025-11-22 20:57:09 -0600

    Updating Nuxt version

--- app/stores/blocks.ts
+++ app/stores/blocks.ts
@@ -1,10 +1,9 @@
-import type { Block } from 'nimiq-rpc-client-ts'
+import type { Block } from 'nimiq-rpc-client-ts/types'
 import { createIdenticon } from 'identicons-esm'
-import { BlockType } from 'nimiq-rpc-client-ts'
+import { BlockType } from 'nimiq-rpc-client-ts/types'
 
 export const useBlocks = defineStore('blocks', () => {
   const url = '/api/blocks'
-  console.log(url)
   const blocksStr = ref<string>('')
   const eventSource = new EventSource(url)
   eventSource.onmessage = (event) => {
@@ -20,7 +19,7 @@ export const useBlocks = defineStore('blocks', () => {
     if (b.type === BlockType.Macro)
       return
     micro.value = b
-    svg.value = await createIdenticon(b.producer.validator)
+    svg.value = createIdenticon(b.producer.validator)
   })
   return {
     svg,
---
+++ server/api/blocks.ts
@@ -0,0 +1,52 @@
+import type { Block } from 'nimiq-rpc-client-ts/types'
+import { initRpcClient } from 'nimiq-rpc-client-ts/client'
+import { BlockType } from 'nimiq-rpc-client-ts/types'
+import { subscribeForHeadBlock } from 'nimiq-rpc-client-ts/ws'
+
+export interface StreamedBlock {
+  number: number
+  epoch: number
+  batch: number
+  validator?: string
+}
+
+export default defineEventHandler(async (event) => {
+  const eventStream = createEventStream(event)
+
+  const config = useRuntimeConfig()
+  const nodeRpcUrl = config.nimiqRpcUrl
+
+  try {
+    initRpcClient({ url: nodeRpcUrl })
+
+    const eventEmitter = await subscribeForHeadBlock(true)
+
+    eventEmitter.addEventListener('data', async (event: CustomEvent) => {
+      const { data: block } = event.detail as { data: Block | null }
+      if (!block)
+        return
+
+      const streamedBlock: StreamedBlock = {
+        number: block.number,
+        batch: block.batch,
+        epoch: block.epoch,
+        validator: block.type === BlockType.Micro ? block.producer.validator : undefined,
+      }
+
+      eventStream.push(JSON.stringify({ type: 'block', data: streamedBlock }))
+    })
+
+    eventEmitter.addEventListener('error', () => {
+      eventStream.push(JSON.stringify({ type: 'error', message: 'Subscription error' }))
+    })
+  }
+  catch {
+    eventStream.push(JSON.stringify({ type: 'error', message: 'Failed to subscribe' }))
+  }
+
+  eventStream.onClosed(async () => {
+    await eventStream.close()
+  })
+
+  return eventStream.send()
+})
