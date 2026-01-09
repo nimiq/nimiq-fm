21a85027604dc9ea6d133b2c233f338f665da7d7
onmax <maximogarciamtnez@gmail.com>
2026-01-09 11:59:45 +0100

    add seed nodes config like wallet, use pico sync

--- app/plugins/nimiq-client.ts
+++ app/plugins/nimiq-client.ts
@@ -8,15 +8,36 @@ declare module '#app' {
   }
 }
 
+const SEED_NODES = [
+  '/dns4/aurora.seed.nimiq.com/tcp/443/wss',
+  '/dns4/catalyst.seed.nimiq.network/tcp/443/wss',
+  '/dns4/cipher.seed.nimiq-network.com/tcp/443/wss',
+  '/dns4/eclipse.seed.nimiq.cloud/tcp/443/wss',
+  '/dns4/lumina.seed.nimiq.systems/tcp/443/wss',
+  '/dns4/nebula.seed.nimiq.com/tcp/443/wss',
+  '/dns4/nexus.seed.nimiq.network/tcp/443/wss',
+  '/dns4/polaris.seed.nimiq-network.com/tcp/443/wss',
+  '/dns4/photon.seed.nimiq.cloud/tcp/443/wss',
+  '/dns4/pulsar.seed.nimiq.systems/tcp/443/wss',
+  '/dns4/quasar.seed.nimiq.com/tcp/443/wss',
+  '/dns4/solstice.seed.nimiq.network/tcp/443/wss',
+  '/dns4/vortex.seed.nimiq.cloud/tcp/443/wss',
+  '/dns4/zenith.seed.nimiq.systems/tcp/443/wss',
+]
+
 async function initializeClient(): Promise<Client | null> {
   try {
-    const { default: init, Client } = await import('@nimiq/core/web')
+    const { default: init, Client, ClientConfiguration } = await import('@nimiq/core/web')
 
     await init()
 
-    const client = await Client.create({
-      logLevel: 'info',
-    })
+    const config = new ClientConfiguration()
+    config.network('mainalbatross')
+    config.seedNodes(SEED_NODES)
+    config.syncMode('pico')
+    config.logLevel('info')
+
+    const client = await Client.create(config.build())
 
     return client
   }
